8a0371e2856b2040b3026711b176ab141b24b223
Angela Sun <angelasun1995@gmail.com>
2022-04-27 19:53:03 -0400

    Initialized state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      CV: {
+        general: {
+          name: '',
+          email: '',
+          phone: '',
+          description: '',
+        },
+        educationList: [],
+        education: {
+          schoolName: '',
+          titleOfStudy: '',
+          dateOfStudyStart: '',
+          dateOfStudyEnd: '',
+        },
+        experienceList: [],
+        experience: {
+          companyName: '',
+          positionTitle: '',
+          tasks: '',
+          dateOfEmploymentStart: '',
+          dateOfEmploymentEnd: '',
+        },
+      },
+    };
+  }
+
   render() {
-    return <Form />;
+    return <Form info={this.state.CV} />;
   }
 }
 
