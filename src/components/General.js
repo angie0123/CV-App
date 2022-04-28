2255c17ff8adcc347fc9932724483b564bd05593
Angela Sun <angelasun1995@gmail.com>
2022-04-27 21:16:43 -0400

    Add styling to general section

--- src/components/General.js
+++ src/components/General.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import Subheading from './Subheading';
 class General extends Component {
   render() {
     const { name, email, phone, description } = this.props;
     return (
       <>
+        <Subheading text="Personal Information" />
         <input type="text" placeholder="Name" value={name} />
         <input type="email" placeholder="Email" value={email} />
         <input type="phone" placeholder="Phone number" value={phone} />
---
+++ src/components/Subheading.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Subheading extends Component {
+  render() {
+    return <div className="subheading">{this.props.text} </div>;
+  }
+}
+
+export default Subheading;
