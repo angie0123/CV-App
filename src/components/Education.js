dd377d31f6a7fa10ee29dfd36ae84006e3151d57
Angela Sun <angelasun1995@gmail.com>
2022-04-27 22:10:02 -0400

    Styled education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -26,26 +26,28 @@ class Education extends Component {
           placeholder="Title of study"
           onChange={inputChangeHandler}
         />
-        <div className="input-container">
-          <label htmlFor="studyDateStart">Date Start</label>
-          <input
-            id="studyDateStart"
-            type="date"
-            name="education dateOfStudyStart"
-            value={dateOfStudyStart}
-            placeholder="Start date"
-            onChange={inputChangeHandler}
-          />
-        </div>
-        <div className="input-container">
-          <label htmlFor="studyDateEnd">Date End</label>
-          <input
-            id="studyDateEnd"
-            type="date"
-            name="education dateofStudyEnd"
-            value={dateofStudyEnd}
-            onChange={inputChangeHandler}
-          />
+        <div className="dates-container">
+          <div className="input-container">
+            <label htmlFor="studyDateStart">Date Start</label>
+            <input
+              id="studyDateStart"
+              type="date"
+              name="education dateOfStudyStart"
+              value={dateOfStudyStart}
+              placeholder="Start date"
+              onChange={inputChangeHandler}
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="studyDateEnd">Date End</label>
+            <input
+              id="studyDateEnd"
+              type="date"
+              name="education dateofStudyEnd"
+              value={dateofStudyEnd}
+              onChange={inputChangeHandler}
+            />
+          </div>
         </div>
       </>
     );
